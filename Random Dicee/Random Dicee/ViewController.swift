0b5083e68d71d2121fb7be1418671a0bca48f624
OtarGelitashvili <otargelitashvili@gmail.com>
2020-10-09 17:01:22 +0400

    Commit

---
+++ Random Dicee/Random Dicee/ViewController.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var diceImageView1: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad();
+        
+        
+    }
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+       let diceArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ];
+        diceImageView1.image = diceArray.randomElement();
+        diceImageView2.image = diceArray.randomElement();
+    }
+}
